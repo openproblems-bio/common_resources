aa24e19ea87936fba5e120e54ae1b8cbd6ee9471
Kai Waldrant <kai@data-intuitive.com>
2024-07-05 20:45:28 +0200

    WIP test

--- src/create_component/test.py
+++ src/create_component/test.py
@@ -8,45 +8,47 @@ meta = {
 }
 ## VIASH END
 
-print(meta["resources_dir"])
+print(meta['executable'])
 
 
 task_template = "/tmp/task_template"
 output_path = f"{task_template}/src/methods/test_method"
 
-cmd = [
-    meta['executable'],
-    '--task', 'task_template',
-    '--type', 'method',
-    '--name', 'test_method',
-    '--language', 'python'
-]
+assert path.exists(task_template), "Task template does not exist"
 
-print('>> Running the script as test', flush=True)
-out = subprocess.run(cmd, stderr=subprocess.STDOUT, cwd=task_template)
+# cmd = [
+#     meta['executable'],
+#     '--task', 'label_projection',
+#     '--type', 'method',
+#     '--name', 'test_method',
+#     '--language', 'python'
+# ]
 
-if out.stdout:
-    print(out.stdout)
+# print('>> Running the script as test', flush=True)
+# out = subprocess.run(cmd, stderr=subprocess.STDOUT, cwd=opv2)
 
-if out.returncode:
-    print(f"script: '{cmd}' exited with an error.")
-    exit(out.returncode)
+# if out.stdout:
+#     print(out.stdout)
 
-print('>> Checking whether output files exist', flush=True)
-assert path.exists(output_path), "Output dir does not exist"
+# if out.returncode:
+#     print(f"script: '{cmd}' exited with an error.")
+#     exit(out.returncode)
 
-conf_f = path.join(output_path, 'config.vsh.yaml')
-assert path.exists(conf_f), "Config file does not exist"
+# print('>> Checking whether output files exist', flush=True)
+# assert path.exists(output_path), "Output dir does not exist"
 
-script_f = path.join(output_path, "script.py")
-assert path.exists(script_f), "Script file does not exist"
+# conf_f = path.join(output_path, 'config.vsh.yaml')
+# assert path.exists(conf_f), "Config file does not exist"
 
-print('>> Checking file contents', flush=True)
-yaml = YAML(typ='safe', pure=True)
-with open(conf_f) as f:
-    conf_data = yaml.load(f)
+# script_f = path.join(output_path, "script.py")
+# assert path.exists(script_f), "Script file does not exist"
 
-assert conf_data['name'] == 'test_method', "Name should be equal to 'test_method'"
+# print('>> Checking file contents', flush=True)
+# yaml = YAML(typ='safe', pure=True)
+# with open(conf_f) as f:
+#     conf_data = yaml.load(f)
+
+# assert conf_data['name'] == 'test_method', "Name should be equal to 'test_method'"
 
 
 print('All checks succeeded!', flush=True)
