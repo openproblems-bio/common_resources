8f4e3addd1fc1c703920d482f511c34c1bca901c
Kai Waldrant <kai@data-intuitive.com>
2024-08-08 11:31:25 +0200

    [WIP] update test create_readme

--- src/create_task_readme/test.R
+++ src/create_task_readme/test.R
@@ -3,28 +3,28 @@ requireNamespace("assertthat", quietly = TRUE)
 ## VIASH START
 ## VIASH END
 
-opv2 <- paste0(meta$resources_dir, "/openproblems-v2")
+task_template <- "/task_template"
 output_path <- "output.md"
+setwd(task_template)
+system("ls -l ")
+# cat(">> Running the script as test\n")
+# system(paste(
+#   meta["executable"],
+#   "--output", output_path,
+#   "--task_dir", paste0(task_template, "/src"),
+#   "--viash_yaml", paste0(task_template, "/_viash.yaml")
+# ))
 
-cat(">> Running the script as test\n")
-system(paste(
-  meta["executable"],
-  "--task", "label_projection",
-  "--output", output_path,
-  "--task_dir", paste0(opv2, "/src/tasks/label_projection"),
-  "--viash_yaml", paste0(opv2, "/_viash.yaml")
-))
+# cat(">> Checking whether output files exist\n")
+# assertthat::assert_that(file.exists(output_path))
 
-cat(">> Checking whether output files exist\n")
-assertthat::assert_that(file.exists(output_path))
-
-cat(">> Checking file contents\n")
-lines <- readLines(output_path)
-assertthat::assert_that(any(grepl("# Label projection", lines)))
-assertthat::assert_that(any(grepl("# Description", lines)))
-assertthat::assert_that(any(grepl("# Motivation", lines)))
-assertthat::assert_that(any(grepl("# Authors", lines)))
-assertthat::assert_that(any(grepl("flowchart LR", lines)))
-assertthat::assert_that(any(grepl("# File format:", lines)))
+# cat(">> Checking file contents\n")
+# lines <- readLines(output_path)
+# assertthat::assert_that(any(grepl("# Task Template ", lines)))
+# assertthat::assert_that(any(grepl("# Description", lines)))
+# assertthat::assert_that(any(grepl("# Motivation", lines)))
+# assertthat::assert_that(any(grepl("# Authors", lines)))
+# assertthat::assert_that(any(grepl("flowchart LR", lines)))
+# assertthat::assert_that(any(grepl("# File format:", lines)))
 
 cat("All checks succeeded!\n")
